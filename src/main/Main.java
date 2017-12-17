60d7e1fcd822cb693a60726380c15a0faff5971e
MSI-SOBREMESA <git@bitbucket.org:plaroser/pruebaandroid.git>
2017-12-17 12:40:08 +0100

    Main refactorizado

--- src/main/Main.java
+++ src/main/Main.java
@@ -12,23 +12,8 @@ import controller.Controller;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		// Empleado e = new Empleado(666666666, "nombre", "Apellidos", "DD/MM/AAAA",
-		// "Departamento", "startDate",
-		// new ArrayList<String>(), GruposSanguineos.A_NEGATIVO.toString());
-		// Agenda a = new Agenda(e, new ArrayList<Contacto>(),
-		// TipoDeArchivos.json.toString());
-		//
-		// XStream xstream = new XStream((new JsonHierarchicalStreamDriver()));
-		// xstream.setMode(XStream.NO_REFERENCES);
-		// xstream.alias(Agenda.ALIAS, Agenda.class);
-		// String json = xstream.toXML(a);
-		// Writer writer = new BufferedWriter(new OutputStreamWriter(new
-		// FileOutputStream("c:\\agendas\\666666666.json")));
-		// writer.write(json);
-		// writer.close();
-		Controller controller = new Controller();
-			controller.startProgram();
-		
+		new Controller();
+
 	}
 
 }
